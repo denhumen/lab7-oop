e8a7d3315a76289e3c977ca89e2c49067dd862bc
Denis Humeniuk <116521940+denhumen@users.noreply.github.com>
2023-10-27 00:23:20 +0300

    Update BasketDecorator.java

--- src/main/java/ua/edu/ucu/apps/flowerstore/flowers/decorators/BasketDecorator.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/flowers/decorators/BasketDecorator.java
@@ -6,12 +6,10 @@ public class BasketDecorator extends ItemDecorator {
 
     public Item item;
 
-    @Override
     public String getDescription() {
         return "Description";
     }
 
-    @Override
     public double getPrice() {
         return 0;
     }
